f18c0177797575f44d503a3a29c3a5b3fa398981
Elizabeth Polgreen <elizabeth.polgreen@cs.ox.ac.uk>
2016-03-17 18:34:45 +0000

    checkproperty checks the property (true U<4 state2). statmodelcheck() returns the percentage of strings that satisfy the property

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <vector>
 #include <cassert>
 #include <random>
+#include <algorithm>
+#include <iostream>
+
 typedef std::vector<unsigned> tracet; 
 enum resultt {UNKNOWN, PASS, FAIL};
 tracet gettrace(std::default_random_engine &);
@@ -12,7 +15,7 @@ unsigned tracelength = 10;
 void statmodelcheck()
 {
 	std::default_random_engine generator;
-	
+    float prob=0.5;
 	unsigned n=0, p=0, f=0;
 	tracet sigma;
 	while(n<N)
@@ -27,7 +30,9 @@ void statmodelcheck()
 		}
 
 	}
-
+    prob = (float)p/((float)p+(float)f);
+    std::cout<< "Property is satisfied with probability " << prob <<"\nNumber of strings satisfying property is " << p << "\nNumber of strings failing property is "<<f <<"\n";
+    
 
 
 }
@@ -45,7 +50,9 @@ tracet gettrace(std::default_random_engine &generator)
 		switch(state)
 		{	
 			case 0: state  = distribution(generator); break;
-			case 1: break;
+            case 1: state = distribution(generator)+ 1;break;
+            case 2: state = distribution(generator)+2; break;
+            case 3: break;
 			default: assert(0); //unexpected state
 		}
 		trace.push_back(state);
@@ -55,7 +62,25 @@ tracet gettrace(std::default_random_engine &generator)
 
 resultt checkproperty(tracet trace)
 {
-	return FAIL;
+    //property = (true U<4 "state2"), i.e., reach state 3 within 4 steps.
+    std::vector<unsigned>::iterator it = std::find(trace.begin(), trace.end(), 3);
+    
+    if(it == trace.end())
+    {
+        std::cout << "Property not satisfied, did not reach state 2 \n";
+        return FAIL;
+    }
+    else if (*it<4)
+    {
+        std::cout << "Satisfies property, reached state 2 in position " << *it << "\n";
+        return PASS;
+    }
+    else
+    {
+        std::cout<<"Property not satisfied, reached state 2 in position "<< *it << "\n";
+        return FAIL;
+    }
+	
 }
 
 
